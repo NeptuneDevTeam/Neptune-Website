c502aed4dbcfb368e6adc4f0a04c4afba7f3df97
THEBRYANSMITH <bryanstechinfo@gmail.com>
2025-01-14 14:59:18 -0500

    Defined rest of Abstract User Entity

--- src/Backend/src/main/java/NeptuneWebsite/Backend/model/User.java
+++ src/Backend/src/main/java/NeptuneWebsite/Backend/model/User.java
@@ -17,7 +17,50 @@ public abstract class User {
     @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Long userId;
 
+
     private String name;
     private Long school;
     private Boolean accountStatus;
+
+    public User() {
+    }
+
+    public User(Long userId, String name, Long school, Boolean accountStatus) {
+        this.userId = userId;
+        this.name = name;
+        this.school = school;
+        this.accountStatus = accountStatus;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getSchool() {
+        return school;
+    }
+
+    public void setSchool(Long school) {
+        this.school = school;
+    }
+
+    public Boolean getAccountStatus() {
+        return accountStatus;
+    }
+
+    public void setAccountStatus(Boolean accountStatus) {
+        this.accountStatus = accountStatus;
+    }
 }
