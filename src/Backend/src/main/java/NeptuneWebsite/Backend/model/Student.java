5a07f26ee113fc1a01b021cc00cfd59c3b2ca6a5
will chapman <williamchapman53@gmail.com>
2025-01-14 14:52:01 -0500

    added constructor and getters/setters to Student model

--- src/Backend/src/main/java/NeptuneWebsite/Backend/model/Student.java
+++ src/Backend/src/main/java/NeptuneWebsite/Backend/model/Student.java
@@ -1,12 +1,75 @@
 package NeptuneWebsite.Backend.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import org.springframework.stereotype.Service;
 
 @Entity
 public class Student {
 
+@Id
+@GeneratedValue(strategy = GenerationType.AUTO)
+private Long id;
 
+private int gradeLevel;
+private boolean iepStatus;
+private boolean adhdStatus;
+private String customSettings;
 
+private Student() {}
 
+private Student(Long id,
+                int gradeLevel,
+                boolean iepStatus,
+                boolean adhdStatus,
+                String customSettings){
+    this.id = id;
+    this.gradeLevel = gradeLevel;
+    this.iepStatus = iepStatus;
+    this.adhdStatus = adhdStatus;
+    this.customSettings = customSettings;
+
+}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getGradeLevel() {
+        return gradeLevel;
+    }
+
+    public void setGradeLevel(int gradeLevel) {
+        this.gradeLevel = gradeLevel;
+    }
+
+    public boolean isIepStatus() {
+        return iepStatus;
+    }
+
+    public void setIepStatus(boolean iepStatus) {
+        this.iepStatus = iepStatus;
+    }
+
+    public boolean isAdhdStatus() {
+        return adhdStatus;
+    }
+
+    public void setAdhdStatus(boolean adhdStatus) {
+        this.adhdStatus = adhdStatus;
+    }
+
+    public String getCustomSettings() {
+        return customSettings;
+    }
+
+    public void setCustomSettings(String customSettings) {
+        this.customSettings = customSettings;
+    }
 }
--- src/Backend/src/main/java/NeptuneWebsite/Backend/repository/StudentRepository.java
+++ src/Backend/src/main/java/NeptuneWebsite/Backend/repository/StudentRepository.java
@@ -2,6 +2,8 @@ package NeptuneWebsite.Backend.repository;
 
 import NeptuneWebsite.Backend.model.Student;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StudentRepository extends CrudRepository<Student, Long> {
 }
